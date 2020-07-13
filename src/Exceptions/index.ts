2fce3ef6eea591952e917a32951d7b3b1867c70c
João Lenon <lenonsec7@gmail.com>
2020-07-13 15:02:03 -0300

    feat: Exceptions

---
+++ src/Exceptions/index.ts
@@ -0,0 +1,4 @@
+export { NotFoundException } from './NotFoundException'
+export { UnauthorizedException } from './UnauthorizedException'
+export { InternalErrorException } from './InternalErrorException'
+export { InvalidArgsException } from './InvalidArgsException'
