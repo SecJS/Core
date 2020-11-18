3e6f0f74c80334ebca3538a4a63285b61abd7286
João Lenon <lenonsec7@gmail.com>
2020-11-18 20:17:27 -0300

    refactor: import nestjs stuffs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { fillable } from './Utils/fillable'
 import { sleep } from './Utils/sleep'
 import { sort } from './Utils/sort'
 import { Token } from './Utils/Token'
+import { NestCrudService } from './NestJS/Services/NestCrudService'
+import { NestCrudController } from './NestJS/Controllers/NestCrudController'
+import { NestCrudRepository } from './NestJS/Repositories/NestCrudRepository'
 
 export {
     sort,
@@ -9,4 +12,7 @@ export {
     fillable,
 
     Token,
+    NestCrudService,
+    NestCrudController,
+    NestCrudRepository,
 }
