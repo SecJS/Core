3b32dc45dd6dfd8d72437c707f86f67cfea12e26
João Lenon <lenonsec7@gmail.com>
2020-11-18 11:46:03 -0300

    examples: Add examples to NestJS Core

--- src/NestJS/Repositories/NestCrudRepository.ts
+++ src/NestJS/Repositories/NestCrudRepository.ts
@@ -1,4 +1,14 @@
-// export class NestCrudRepository {
+// import { EntityRepository, Repository } from 'typeorm'
+//
+// @EntityRepository(User)
+// export class NestCrudRepository extends Repository<User> {
+//     protected model: any
+
+//     constructor(resource: any) {
+//         this.model = resource
+//     }
+//     
 //     public async getResource(id: string | number) {}
 //     public async getResources(id: string | number) {}
 // }
+
