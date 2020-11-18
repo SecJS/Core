4ac25bf89828b017d6ff2471319e1765375788c5
João Lenon <lenonsec7@gmail.com>
2020-11-18 11:35:02 -0300

    feat: Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { fillable } from './Utils/fillable'
 import { sleep } from './Utils/sleep'
 import { sort } from './Utils/sort'
-import { Token } from './Utils/token'
+import { Token } from './Utils/Token'
 
 export {
     sort,
