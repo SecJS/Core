7d4e2ae996e368dfe9c860852d8e8ebd2420f22f
João Lenon <lenonsec7@gmail.com>
2020-11-21 17:27:56 -0300

    feat: stringToNumber Parser

--- src/Utils/Classes/Parser.ts
+++ src/Utils/Classes/Parser.ts
@@ -1,5 +1,30 @@
+export interface IStringToNumber {
+  string: string | number,
+  isCordinate?: boolean
+}
+
 export class Parser {
-  public async stringToArray(string: string) {
+  public async stringToArray(string: string): Promise<string[]> {
     return Promise.all(string.split(',').map(index => index.trim()))
+  }
+
+  public async stringToNumber({ string, isCordinate }: IStringToNumber): Promise<number> {
+    if (typeof string === 'string') {
+      if (!string.replace(/\D/g, "")) {
+        throw new Error('Your string is invalid, it should have at least one number')
+      }
+
+      string = string.replace(/\D/g, "")
+    }
+
+    if (typeof string === 'number') {
+      return string
+    }
+
+    if (string.length >= 9 || isCordinate) {
+      return parseFloat(string)
+    }
+
+    return parseInt(string)
   }
 }
