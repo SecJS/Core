72bc5bb2c9f96010eb071bc9cf4ec9cc0789e802
João Lenon <lenonsec7@gmail.com>
2020-11-21 17:27:45 -0300

    feat: export new implementations

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { sort } from './Utils/Functions/sort'
 import { sleep } from './Utils/Functions/sleep'
 import { random } from './Utils/Functions/random'
 import { fillable } from './Utils/Functions/fillable'
+import { deg2rad, kmRadius, ICoordinate } from './Utils/Functions/kmRadius'
 
 import { Token } from './Utils/Classes/Token'
-import { Parser } from './Utils/Classes/Parser'
+import { Parser, IStringToNumber } from './Utils/Classes/Parser'
 import { NestCrudService } from './NestJS/Services/NestCrudService'
 import { NestCrudController } from './NestJS/Controllers/NestCrudController'
 import { NestCrudRepository } from './NestJS/Repositories/NestCrudRepository'
@@ -13,10 +14,14 @@ export {
     sort,
     sleep,
     random,
+    deg2rad,
     fillable,
+    kmRadius,
 
     Token,
     Parser,
+    ICoordinate,
+    IStringToNumber,
     NestCrudService,
     NestCrudController,
     NestCrudRepository,
